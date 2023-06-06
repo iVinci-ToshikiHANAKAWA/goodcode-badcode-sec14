8fdb328ea0651fce5c459a35f392345c18ccee55
iVinci-ToshikiHANAKAWA <toshiki.hanakawa@i-Vinci.co.jp>
2023-06-06 20:28:55 +0900

    managerに委譲

--- DeliveryCharge.java
+++ DeliveryCharge.java
@@ -1,15 +1,9 @@
-package chapter14_refactoring.delivery.v3;
+package chapter14_refactoring.delivery.v4;
 
 class DeliveryCharge {
   final int amount;
 
   DeliveryCharge(final ShoppingCart shoppingCart) {
-    int totalPrice = shoppingCart.products.get(0).price + shoppingCart.products.get(1).price;
-    if (totalPrice < 2000) {
-      amount = 500;
-    }
-    else {
-      amount = 0;
-    }
+    amount = DeliveryManager.deliveryCharge(shoppingCart.products);
   }
 }
